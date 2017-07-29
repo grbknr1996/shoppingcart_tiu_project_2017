b6288e8b87f21cd99664964ce638cfbc7c23ec94
gourab konar <grbknr1996@gmail.com>
2017-07-29 08:18:29 -0500

    commit#6

--- shoppingcart_tiu_project_2017/Areas/Admin/Controllers/ShopController.cs
+++ shoppingcart_tiu_project_2017/Areas/Admin/Controllers/ShopController.cs
@@ -10,47 +10,105 @@ namespace shoppingcart_tiu_project_2017.Areas.Admin.Controllers
 {
     public class ShopController : Controller
     {
-        // GET: Admin/Shop/Categories
-        public ActionResult Categories()
-        {
-            //declare a list of models
-            List<CategoryVM> categoryVMList;
-            using (Db db = new Db())
+        
+       
+            // GET: Admin/Shop/Categories
+            public ActionResult Categories()
             {
-                //init the models
-                categoryVMList = db.Categories.ToArray().OrderBy(x => x.Sorting).Select(x => new CategoryVM(x)).ToList();
-            }
+                // Declare a list of models
+                List<CategoryVM> categoryVMList;
 
-                //return view with models
+                using (Db db = new Db())
+                {
+                    // Init the list
+                    categoryVMList = db.Categories
+                                    .ToArray()
+                                    .OrderBy(x => x.Sorting)
+                                    .Select(x => new CategoryVM(x))
+                                    .ToList();
+                }
 
+                // Return view with list
                 return View(categoryVMList);
-        }
-        // POST: Admin/Shop/AddNewCategories
-        [HttpPost]
-        public string AddNewCategory(string catName)
-        {
-            //declare id
-            string id;
-            using (Db db = new Db())
-            {
-                //check that the category name is unique
-                if (db.Categories.Any(x => x.Name == catName)) return "title taken";
-                //init dto
-                CategoryDTO dto = new CategoryDTO();
-                //add to DTO
-                dto.Name = catName;
-                dto.Slug = catName.Replace(" ", "-").ToLower();
-                dto.Sorting = 100;
-                //save the dto
-                db.Categories.Add(dto);
-                db.SaveChanges();
-
-                //get the id
-                id = dto.Id.ToString();
             }
-            //return the id
-            return id;
-                
+
+            // POST: Admin/Shop/AddNewCategory
+            [HttpPost]
+            public string AddNewCategory(string catName)
+            {
+                // Declare id
+                string id;
+
+                using (Db db = new Db())
+                {
+                    // Check that the category name is unique
+                    if (db.Categories.Any(x => x.Name == catName))
+                        return "titletaken";
+
+                    // Init DTO
+                    CategoryDTO dto = new CategoryDTO();
+
+                    // Add to DTO
+                    dto.Name = catName;
+                    dto.Slug = catName.Replace(" ", "-").ToLower();
+                    dto.Sorting = 100;
+
+                    // Save DTO
+                    db.Categories.Add(dto);
+                    db.SaveChanges();
+
+                    // Get the id
+                    id = dto.Id.ToString();
+                }
+
+                // Return id
+                return id;
+            }
+
+            // POST: Admin/Shop/ReorderCategories
+            [HttpPost]
+            public void ReorderCategories(int[] id)
+            {
+                using (Db db = new Db())
+                {
+                    // Set initial count
+                    int count = 1;
+
+                    // Declare CategoryDTO
+                    CategoryDTO dto;
+
+                    // Set sorting for each category
+                    foreach (var catId in id)
+                    {
+                        dto = db.Categories.Find(catId);
+                        dto.Sorting = count;
+
+                        db.SaveChanges();
+
+                        count++;
+                    }
+                }
+
+            }
+
+            // GET: Admin/Shop/DeleteCategory/id
+            public ActionResult DeleteCategory(int id)
+            {
+                using (Db db = new Db())
+                {
+                    // Get the category
+                    CategoryDTO dto = db.Categories.Find(id);
+
+                    // Remove the category
+                    db.Categories.Remove(dto);
+
+                    // Save
+                    db.SaveChanges();
+                }
+
+                // Redirect
+                return RedirectToAction("Categories");
+            }
+
         }
     }
-}
